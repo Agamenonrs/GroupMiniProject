a9ef9c8585c7903e60bd18c643c6b98d21d66ff8
Agamenonrs <agamenonrsrj@gmail.com>
2020-03-19 15:43:33 -0300

    Refactory driver

--- src/Driver.java
+++ src/Driver.java
@@ -17,9 +17,15 @@ public class Driver {
                 input = InputCollector.getUserInput("Enter UCI (type 'Help' for help):");
 
                 if(input !=null && !input.equals("")){
-                    if(input.equalsIgnoreCase("exit")){
+
+                    if(ApplicationUtils.regexValitation(input,ApplicationUtils.UCI_PATTERN)) {
+                        int[] typedPositions = convertToIntegerArray(input);
+                        Position oldPosition = new Position(typedPositions[0], typedPositions[1]);
+                        Position newPosition = new Position(typedPositions[2], typedPositions[3]);
+                        game.changePosition(oldPosition, newPosition, game);
                         continue;
                     }
+
                     if(HelpOptions.HELP.getCode().equalsIgnoreCase(input)){
                         game.printHelOptions();
                         continue;
@@ -40,14 +46,9 @@ public class Driver {
                         System.out.println(HelpOptions.SQUARE.getDescription());
                         continue;
                     }
-                    if(!ApplicationUtils.regexValitation(input,ApplicationUtils.UCI_PATTERN)){
-                        System.out.println("Wrong code. Try again");
-                        continue;
-                    }
-                    int[] typedPositions = convertToIntegerArray(input);
-                    Position oldPosition = new Position(typedPositions[0],typedPositions[1]);
-                    Position newPosition = new Position(typedPositions[2],typedPositions[3]);
-                    game.changePosition(oldPosition,newPosition,game);
+
+                    System.out.println("Wrong option. Try 'help' option");
+
                 }
             }catch (InvalidMovement ex){
                 System.out.println(ex.getMessage());
